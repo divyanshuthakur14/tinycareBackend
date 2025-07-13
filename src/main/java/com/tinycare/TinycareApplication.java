88f3e876f62568d81538607537fab9a06f278064
divyanshuthakur14 <singhthakurdivyanshu@gmail.com>
2025-07-13 17:40:28 +0530

    workingVersion

--- src/main/java/com/tinycare/TinycareApplication.java
+++ src/main/java/com/tinycare/TinycareApplication.java
@@ -1,5 +1,6 @@
 package com.tinycare;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,5 +9,10 @@ public class TinycareApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TinycareApplication.class, args);
+	}
+	@PostConstruct
+	public void init() {
+		// This should print your PostgreSQL URL if application.properties is loaded
+		System.out.println("✅ spring.datasource.url = " + System.getProperty("spring.datasource.url"));
 	}
 }
