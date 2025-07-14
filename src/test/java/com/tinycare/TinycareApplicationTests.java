0efdd028147b3a6ccbd6ffce8d37b6a4297c8b9f
divyanshuthakur14 <singhthakurdivyanshu@gmail.com>
2025-07-14 13:00:39 +0530

    newCommit

--- src/test/java/com/tinycare/TinycareApplicationTests.java
+++ src/test/java/com/tinycare/TinycareApplicationTests.java
@@ -2,8 +2,10 @@ package com.tinycare;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource("classpath:application-test.properties")
 class TinycareApplicationTests {
 
 	@Test
